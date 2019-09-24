791d0ba2d6e99e3aa252b13cb4854f5409c2d019
Mike Puskar <puskar49@gmail.com>
2019-09-24 15:32:30 +0000

    Added some debugging prints

--- tests/integration/test_insert_select.py
+++ tests/integration/test_insert_select.py
@@ -5,8 +5,8 @@ from ..context import py3odb
 
 TEST_DATA = (
     (0, 1.2, 'apple'),
-    (49, 3.14159, 'pie'),
-    (-27, -0.0003, 'longer_than_8_characters')
+    (9, 3.14159, 'pie'),
+    (2, -0.0003, 'longer_than_8_characters')
 )
 
 
@@ -25,17 +25,20 @@ def test_insert_select(tmpdir):
     conn.close()
 
     # read from it
+    print(f"Opening {db_file} for reading.")
     conn = py3odb.connect(f"{db_file}")
     cur = conn.cursor()
+    print("Executing SELECT")
     cur.execute(f"SELECT * FROM '{db_file}'")
     data = []
     for row in cur:
+        print(f"Fetched row: {row}")
         data.append(row)
     for index, (int_value, real_value, string_value) in enumerate(TEST_DATA):
         assert int_value == data[index][0]
         assert real_value == pytest.approx(data[index][1])
         assert string_value[:8] == data[index][2]
-    cur.execute(f"SELECT c FROM '{db_file}' WHERE a=49")
+    cur.execute(f"SELECT c FROM '{db_file}' WHERE a=9")
     data = cur.fetchone()
     assert data[0] == "pie"
     conn.close()
