c2d33886318e3107bb74bc9709ef309e92af4712
Mike Puskar <puskar49@gmail.com>
2019-09-26 15:03:17 +0000

    Tested actual values from sample.odb

--- tests/integration/test_read_sample.py
+++ tests/integration/test_read_sample.py
@@ -6,12 +6,12 @@ from ..context import py3odb
 def test_read_sample():
     """Read a sample database."""
     db_file = pathlib.Path(__file__).parent.parent.parent / "resources" / "sample.odb"
-    print(f"Opening {db_file} for reading.")
     conn = py3odb.connect(f"{db_file}")
     cur = conn.cursor()
-    print("Executing SELECT")
-    cur.execute(f"SELECT * FROM '{db_file}'")
-    print("Fetching a row")
-    data = cur.fetchone()
-    print(f"{data}")
+    cur.execute(f"SELECT DISTINCT varno FROM '{db_file}'")
+    varnos = cur.fetchall()
+    assert len(varnos) == 3
+    assert varnos[0][0] == 162
+    assert varnos[1][0] == 241
+    assert varnos[2][0] == 7
     conn.close()
