21bcf892584d5657e1c3ebdaa07bfc84dd6a903e
Mike Puskar <puskar49@gmail.com>
2019-09-28 18:23:58 +0000

    Made underlying __dict__ for Row an OrderedDict

--- py3odb/row.py
+++ py3odb/row.py
@@ -1,11 +1,18 @@
 """Module for Row class."""
 
+from collections import OrderedDict
 from collections.abc import Mapping
 
 
 class Row(Mapping):
     """An immutable sequence object representing an SQL result row."""
     def __init__(self, *args, **kwargs):
+        # As of Python 3.6, for CPython at least, dictionaries are ordered.
+        # However, this is an implementation detail, not a feature.  Default
+        # insert ordering of dict did not become a feature until Python 3.7.
+        # To ensure correct functionality across Python 3.6 implementations,
+        # the underlying __dict__ needs to be an OrderedDict.
+        self.__dict__ = OrderedDict()
         self.__dict__.update(*args, **kwargs)
 
     def __getitem__(self, index):
