b294a6e890fd92a955042dd39ee8372ff6f6bc69
Mike Puskar <puskar49@gmail.com>
2019-09-28 23:35:08 +0000

    Added command-line interface

--- py3odb/cli/main.py
+++ py3odb/cli/main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-"""Main Module for py3odb"""
+"""Main Module for py3odb command-line interface."""
 from argparse import ArgumentParser
 
+from py3odb import __version__
 from py3odb.cli.command import Command
 import py3odb.cli.dump as dump
 import py3odb.cli.geopoints as geopoints
@@ -34,7 +35,8 @@ def usage():
     """Print the usage statement."""
     print("Usage:")
     print("    py3odb <command> [<args>]")
-    print("    py3odb help <command>\n")
+    print("    py3odb help <command>")
+    print("    py3odb --version\n")
     print("Available commands:")
     for keyname, class_name in _COMMANDS.items():
         if class_name.help_text is None:
@@ -43,15 +45,26 @@ def usage():
 
 
 def main():
+    """Main function"""
     parser = ArgumentParser(add_help=False)
-    subparsers = parser.add_subparsers()
-    subparsers.required = True
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="store_true",
+    )
+    subparsers = parser.add_subparsers(dest="command")
     for command_class in _COMMANDS.values():
         command_class(subparsers)
-    try:
-        args = parser.parse_args()
-        args.command(args)
-    except TypeError:
+    args = parser.parse_args()
+    if args.version:
+        print(f"py3odb version {__version__}")
+    elif args.command is not None:
+        try:
+            args.command(args)
+        except KeyboardInterrupt:
+            print("\n")
+            raise SystemExit(1)
+    else:
         usage()
 
 
