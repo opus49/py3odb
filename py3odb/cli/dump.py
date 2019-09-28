2c931d7ca208fbc458901c67091e8009a79ae28e
Mike Puskar <puskar49@gmail.com>
2019-09-28 23:20:38 +0000

    Dump command for py3odb

--- py3odb/cli/dump.py
+++ py3odb/cli/dump.py
@@ -1,5 +1,7 @@
 """Module for the dump command."""
+from py3odb import connect
 from py3odb.cli.command import Command
+from py3odb.constants import ColumnType, Varno
 
 
 class DumpCommand(Command):
@@ -8,8 +10,24 @@ class DumpCommand(Command):
 
     def add_arguments(self):
         super().add_arguments()
-        self.parser.add_argument("-c", "--c", help="Print the columns.", action="store_true")
-        self.parser.add_argument("-v", "--v", help="Print the varnos.", action="store_true")
+        self.parser.add_argument(
+            "-c",
+            "--columns",
+            help="Print the columns.",
+            action="store_true"
+        )
+        self.parser.add_argument(
+            "-v",
+            "--varno",
+            help="Print the varnos.",
+            action="store_true"
+        )
+        self.parser.add_argument(
+            "-vv",
+            "--verbose",
+            help="Print full details.",
+            action="store_true"
+        )
 
     @property
     def description(self):
@@ -21,6 +39,63 @@ class DumpCommand(Command):
         main(args)
 
 
+def print_columns(filename, verbose=False):
+    """Print the columns from an ODB2 file."""
+    conn = connect(filename)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM <odb>")
+    columns = {}
+    for column in cur.description:
+        columns[column[0]] = ColumnType(column[1]).name
+    if verbose:
+        print(f"{'name':40}type")
+        print("-" * 51)
+        for column_name in sorted(columns):
+            print(f"{column_name:40}{columns[column_name]}")
+    else:
+        print("name")
+        print("-" * 51)
+        for column_name in sorted(columns):
+            print(f"{column_name}")
+    conn.close()
+
+
+def _get_varno_data(filename):
+    """Retrieve a sorted list of tuples containing varno, code, desc."""
+    varno_data = []
+    conn = connect(filename)
+    cur = conn.cursor()
+    cur.execute("SELECT DISTINCT varno@body FROM <odb>")
+    for row in cur:
+        varno = row["varno@body"]
+        code = Varno.get_code(varno)
+        desc = Varno.get_desc(code) if code != "unknown" else "unknown"
+        varno_data.append((varno, code, desc))
+    conn.close()
+    if varno_data:
+        varno_data.sort(key=lambda x: x[1])
+    return varno_data
+
+
+def print_varnos(filename, verbose=False):
+    """Print a list of varnos from an ODB2 file."""
+    varno_data = _get_varno_data(filename)
+    if verbose:
+        print(f"{'code':19}varno description")
+        print("-" * 80)
+    else:
+        print(f"{'code':20}varno")
+        print("-" * 25)
+    for varno, code, desc in varno_data:
+        if verbose:
+            print(f"{code:20}{varno:-4} {desc:55}")
+        else:
+            print(f"{code:20}{varno:-4}")
+
+
 def main(args):
     """Main function."""
-    print("Running the dump command...")
+    if args.columns:
+        print_columns(args.filename, verbose=args.verbose)
+    if args.varno:
+        print_varnos(args.filename, verbose=args.verbose)
