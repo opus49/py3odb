71600bbed5e6d66ce2b60a3723776c612cc1a0be
Mike Puskar <puskar49@gmail.com>
2019-10-01 14:53:43 +0000

    Added tests for query command exception handling

--- tests/unit/cli/test_query.py
+++ tests/unit/cli/test_query.py
@@ -1,5 +1,6 @@
 """Tests for cli/query module."""
 from argparse import Namespace
+import pytest
 from ...context import py3odb
 
 
@@ -22,3 +23,26 @@ def test_command(mock_reader_select_all, query_command, capsys):
     assert lines[0] == "(23.1, 120.3, 1, 3.2)"
     assert lines[1] == "(-13.2, -10.3, 2, 7.8)"
     assert lines[2] == "(3.8, 40.2, 3, -1.2)"
+
+
+def test_query_handles_missing_file(query_command):
+    """Test that the query command handles missing files."""
+    try:
+        query_command.command(
+            Namespace(filename="doesnotexist.odb", sql_command="foo")
+        )
+    except py3odb.Error:
+        pytest.fail("Failed to handle interface error.")
+
+
+def test_query_handles_programming_error(query_command):
+    """Test that the query command handles non-ODB2 files or syntax errors."""
+    try:
+        query_command.command(
+            Namespace(filename=f"{__file__}", sql_command="SELECT * FROM <odb>")
+        )
+        query_command.command(
+            Namespace(filename=f"{__file__}", sql_command="I like cheese")
+        )
+    except py3odb.Error:
+        pytest.fail("Failed to handle programming error.")
