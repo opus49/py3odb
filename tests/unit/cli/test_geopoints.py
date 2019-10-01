8d18272524d95bc77f0e75efe0aafff83a766034
Mike Puskar <puskar49@gmail.com>
2019-10-01 15:04:04 +0000

    Added tests for geopoints exception handling

--- tests/unit/cli/test_geopoints.py
+++ tests/unit/cli/test_geopoints.py
@@ -1,4 +1,6 @@
 """Tests for cli/query module."""
+from argparse import Namespace
+import pytest
 from ...context import py3odb
 
 
@@ -11,3 +13,23 @@ def test_geopoints_command(geopoints_command):
 def test_description(geopoints_command):
     """Test the GeopointsCommand description property."""
     assert geopoints_command.description == py3odb.cli.geopoints.GeopointsCommand.desc_text
+
+
+def test_geopoints_handles_missing_file(geopoints_command):
+    """Test that the geopoints command handles non-ODB2 files."""
+    try:
+        geopoints_command.command(
+            Namespace(filename="doesnotexist.odb", column="foo", varno=-1)
+        )
+    except py3odb.Error:
+        pytest.fail("Failed to handle interface error.")
+
+
+def test_geopoints_handles_invalid_file(geopoints_command):
+    """Test that the geopoints command handles non-ODB2 files."""
+    try:
+        geopoints_command.command(
+            Namespace(filename=f"{__file__}", column="foo", varno=-1)
+        )
+    except py3odb.Error:
+        pytest.fail("Failed to handle programming error.")
